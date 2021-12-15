c4f72046b9d2dd9a051ecea9a671bcf6d2cf8c94
Vik1ang <vik1ang@outlook.com>
2021-12-15 10:08:31 +0800

    binary tree

--- algorithm/src/main/java/dataStructure/binaryTree/README.md
+++ algorithm/src/main/java/dataStructure/binaryTree/README.md
@@ -300,3 +300,38 @@ public class Solution {
     }
 }
 ```
+
+迭代
+
+```java
+public class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[0]);
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+
+        int inorderIndex = 0;
+        for (int i = 1; i < preorder.length; i++) {
+            int preorderVal = preorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[inorderIndex]) {
+                node.left = new TreeNode(preorderVal);
+                stack.push(node.left);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
+                    node = stack.pop();
+                    inorderIndex++;
+                }
+                node.right = new TreeNode(preorderVal);
+                stack.push(node.right);
+            }
+        }
+
+        return root;
+    }
+}
+```
---
+++ algorithm/src/main/java/dataStructure/binaryTree/q105/buildTree/Solution2.java
@@ -0,0 +1,37 @@
+package dataStructure.binaryTree.q105.buildTree;
+
+import dataStructure.binaryTree.TreeNode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution2 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[0]);
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+
+        int inorderIndex = 0;
+        for (int i = 1; i < preorder.length; i++) {
+            int preorderVal = preorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[inorderIndex]) {
+                node.left = new TreeNode(preorderVal);
+                stack.push(node.left);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
+                    node = stack.pop();
+                    inorderIndex++;
+                }
+                node.right = new TreeNode(preorderVal);
+                stack.push(node.right);
+            }
+        }
+
+        return root;
+    }
+}
