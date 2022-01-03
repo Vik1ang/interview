2fbb67708093e5ddf0116eba32ddc6aa1b4f43b4
Vik1ang <vik1ang@outlook.com>
2022-01-03 21:12:59 +0800

    hot100

---
+++ algorithm/src/main/java/top100/q1/Solution.java
@@ -0,0 +1,23 @@
+package top100.q1;
+
+import java.util.HashMap;
+
+// https://leetcode-cn.com/problems/two-sum/
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int other = target - nums[i];
+            if (map.containsKey(other) && map.get(other) != i) {
+                return new int[]{i, map.get(other)};
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+}
