c87636ba86daffe387b415acd0cea46b9b523215
Vik1ang <vik1ang@outlook.com>
2022-01-09 16:31:24 +0800

    hot100

---
+++ algorithm/src/main/java/top100/q53/Solution.java
@@ -0,0 +1,23 @@
+package top100.q53;
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
+        }
+
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, dp[i]);
+        }
+
+        return res;
+    }
+}
