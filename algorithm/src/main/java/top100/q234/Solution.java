d4e4149e72ae366c60af128405dd5e070f0dc90b
Vik1ang <vik1ang@outlook.com>
2022-01-14 00:28:38 +0800

    hot100

---
+++ algorithm/src/main/java/top100/q234/Solution.java
@@ -0,0 +1,40 @@
+package top100.q234;
+
+import dataStructure.linkedList.ListNode;
+
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        ListNode left = head;
+        ListNode right = reverse(slow);
+        while (right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
---
+++ algorithm/src/main/java/top100/q239/Solution.java
@@ -0,0 +1,34 @@
+package top100.q239;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] != o2[0] ? o2[0] - o1[0] : o2[1] - o1[1];
+            }
+        });
+
+        for (int i = 0; i < k; i++) {
+            priorityQueue.offer(new int[]{nums[i], i});
+        }
+
+        int[] ans = new int[n - k + 1];
+        ans[0] = priorityQueue.peek()[0];
+
+        for (int i = k; i < n; i++) {
+            priorityQueue.offer(new int[]{nums[i], i});
+            while (priorityQueue.peek()[1] <= i - k) {
+                priorityQueue.poll();
+            }
+            ans[i - k + 1] = priorityQueue.peek()[0];
+        }
+
+        return ans;
+
+    }
+}
