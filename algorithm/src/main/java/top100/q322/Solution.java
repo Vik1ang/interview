8f1eb238940ac616fa24eff4cca1b8ad580cd2ac
Vik1ang <vik1ang@outlook.com>
2022-01-14 22:28:12 +0800

    hot100

---
+++ algorithm/src/main/java/top100/q322/Solution.java
@@ -0,0 +1,24 @@
+package top100.q322;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+
+        // base case
+        dp[0] = 0;
+        for (int i = 0; i < dp.length; i++) {
+            for (int coin : coins) {
+                if (i - coin < 0) {
+                    continue;
+                }
+
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+
+        return (dp[amount] == amount + 1) ? -1 : dp[amount];
+    }
+}
