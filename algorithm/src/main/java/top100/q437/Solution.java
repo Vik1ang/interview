6c605c076971297ee4fc0777df6d37d218d5e0ab
Vik1ang <vik1ang@outlook.com>
2022-01-16 14:18:32 +0800

    hot100

---
+++ algorithm/src/main/java/top100/q437/Solution.java
@@ -0,0 +1,42 @@
+package top100.q437;
+
+import dataStructure.binaryTree.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    private Map<Integer, Integer> prefixMap;
+    private int target;
+
+    public int pathSum(TreeNode root, int targetSum) {
+        prefixMap = new HashMap<>();
+        target = targetSum;
+
+        prefixMap.put(0, 1);
+
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode node, int currSum) {
+        if (node == null) {
+            return 0;
+        }
+
+        int res = 0;
+        currSum += node.val;
+
+        res += prefixMap.getOrDefault(currSum - target, 0);
+        prefixMap.put(currSum, prefixMap.getOrDefault(currSum, 0) + 1);
+
+        int left = dfs(node.left, currSum);
+        int right = dfs(node.right, currSum);
+
+        res = res + left + right;
+
+        prefixMap.put(currSum, prefixMap.get(currSum) - 1);
+
+        return res;
+    }
+}
---
+++ algorithm/src/main/java/top100/q438/Solution.java
@@ -0,0 +1,50 @@
+package top100.q438;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        HashMap<Character, Integer> need = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
+
+        for (char c : p.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        ArrayList<Integer> res = new ArrayList<>();
+        int left = 0, right = 0;
+        int valid = 0;
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+
+            while (right - left >= p.length()) {
+                if (valid == need.size()) {
+                    res.add(left);
+                }
+
+                char d = s.charAt(left);
+                left++;
+
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.get(d) - 1);
+                }
+            }
+        }
+
+        return res;
+    }
+}
