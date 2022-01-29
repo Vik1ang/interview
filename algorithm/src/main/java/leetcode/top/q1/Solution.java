b06f4dedc1610333668e48e31c40134d743ee9bc
Vik1ang <vik1ang@outlook.com>
2022-01-29 17:10:07 +0800

    hot

---
+++ algorithm/src/main/java/leetcode/top/q1/Solution.java
@@ -0,0 +1,21 @@
+package leetcode.top.q1;
+
+import java.util.HashMap;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(i, nums[i]);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int other = target - nums[i];
+            if (map.containsKey(other) && map.get(other) != i) {
+                return new int[]{i, map.get(other)};
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+}
